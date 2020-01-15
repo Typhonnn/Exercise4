9d885cbd303dd161c9ec28b985ece40c61a34f14
Typhonnn <46194035+Typhonnn@users.noreply.github.com>
2020-01-15 14:15:30 +0200

    Add project files.

---
+++ Exercise4/Sequence.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_LINE 300
+
+char* createSequence(char* fileName) {
+	FILE *file = NULL;
+	fopen_s(&file, fileName, "w");
+	if (file == NULL) {
+		printf("Failed To Open %s! ABORTING!", fileName);
+		return NULL;
+	}
+	char *line = calloc(MAX_LINE,sizeof(char));
+	if (line == NULL) {
+		printf("Could Not Allocate Line! ABORTING!");
+		return;
+	}
+	char *endFile = NULL;
+	if(!feof(file)) {
+		endFile = fgets(line, MAX_LINE, file);
+	}
+	return line;
+}
+
+char* createSigns(char* SEQ1, char* SEQ2, int n) {
+	if (SEQ1 == NULL) {
+		printf("SEQ1 Is NULL! ABORTING!");
+		return NULL;
+	}
+	if (SEQ2 == NULL) {
+		printf("SEQ2 Is NULL! ABORTING!");
+		return NULL;
+	}
+	char* resultSEQ = calloc(1, sizeof(char));
+	if (resultSEQ == NULL) {
+		printf("Failed To Allocate Memory for resultSEQ! ABORTING!");
+		return NULL;
+	}
+}
